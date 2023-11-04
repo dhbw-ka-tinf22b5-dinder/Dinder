f2956fb35a281d42f3800bb49d610ce278252197
huchsle <janifu02@gmail.com>
2023-11-04 11:41:23 +0100

    erstes frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LoginAndRegistration from "./loginAndRegistration/LoginAndRegistration";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <LoginAndRegistration/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
