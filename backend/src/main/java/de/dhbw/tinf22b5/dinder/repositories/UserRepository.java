5fb3e7e233ee5dd10aeb597a9b3b7d2653cdebd8
js-12345 <schaeferjonathan@gmx.net>
2023-11-21 12:54:32 +0100

    Added a check for forged jwt tokens and removed duplicate methode

--- backend/src/main/java/de/dhbw/tinf22b5/dinder/repositories/UserRepository.java
+++ backend/src/main/java/de/dhbw/tinf22b5/dinder/repositories/UserRepository.java
@@ -1,13 +1,9 @@
 package de.dhbw.tinf22b5.dinder.repositories;
 
 import de.dhbw.tinf22b5.dinder.entities.User;
-import jakarta.annotation.Nullable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    Optional<User> findByEmail(String Email, String UserName);
 }
