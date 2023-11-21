458e7b7c3459ec3768ad39d6752a58eefad88952
Manuel-F-04 <manuel_fam_franz@yahoo.de>
2023-11-21 14:47:46 +0100

    change Database - delete Chat

--- backend/src/main/resources/migrations/V1_0__Create_Tables.sql
+++ backend/src/main/resources/migrations/V1_0__Create_Tables.sql
@@ -10,38 +10,33 @@ CREATE TABLE users
 
 CREATE TABLE advertisement
 (
-    advertisement_id SERIAL       NOT NULL PRIMARY KEY,
+    advertisement_Id SERIAL       NOT NULL PRIMARY KEY,
     title            VARCHAR(255) NOT NULL,
     price            float8       NOT NULL,
     location         VARCHAR(255) NULL,
     postal_code      INT          NOT NULL,
     description      VARCHAR(255) NULL,
     image            VARCHAR(255) NULL,
-    creator_email    VARCHAR(255) NOT NULL REFERENCES users (email) ON DELETE CASCADE ON UPDATE CASCADE,
+    advertiser       VARCHAR(255) NOT NULL REFERENCES users (email) ON DELETE CASCADE ON UPDATE CASCADE,
+    contractor       VARCHAR(255) NULL REFERENCES users (email) ON DELETE CASCADE ON UPDATE CASCADE,
     creation_time    timestamp    NULL
-);
-
-CREATE TABLE chat
-(
-    id               SERIAL       NOT NULL PRIMARY KEY,
-    advertisement_id INT          NOT NULL REFERENCES advertisement (advertisement_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    contractor_email VARCHAR(255) NOT NULL REFERENCES users (email) ON DELETE CASCADE ON UPDATE CASCADE,
-    UNIQUE (advertisement_id, contractor_email)
-);
-
-CREATE TABLE chat_messages
-(
-    message_id   SERIAL       NOT NULL PRIMARY KEY,
-    message      VARCHAR(255) NULL,
-    date_time    timestamp    NOT NULL,
-    chatID       INT          NOT NULL REFERENCES chat (id) ON DELETE CASCADE  ON UPDATE CASCADE,
-    sender_email VARCHAR(255) NOT NULL REFERENCES users (email) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE swipe_information
 (
+    swipe_Id         SERIAL       NOT NULL PRIMARY KEY,
     swipe_time       timestamp    NOT NULL,
-    users_email      VARCHAR(255) NOT NULL REFERENCES users (email),
-    advertisement_id INT          NOT NULL REFERENCES advertisement (advertisement_id),
-    PRIMARY KEY (users_email, advertisement_id)
+    contractor_email VARCHAR(255) NOT NULL REFERENCES users (email),
+    advertisement_Id INT          NOT NULL REFERENCES advertisement (advertisement_Id),
+    UNIQUE (contractor_email,advertisement_Id)
 );
+
+CREATE TABLE chat_message
+(
+    message_id   SERIAL       NOT NULL PRIMARY KEY,
+    message      VARCHAR(255) NULL,
+    date_time    timestamp    NOT NULL,
+    swipe_Id     INT          NOT NULL REFERENCES swipe_information (swipe_Id) ON DELETE CASCADE  ON UPDATE CASCADE,
+    sender_email VARCHAR(255) NOT NULL REFERENCES users (email) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
