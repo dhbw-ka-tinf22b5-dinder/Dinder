b7601b0f3d67505f951eefcf8eba7419c34ed382
traible <traible@apollon.de>
2023-11-21 12:59:30 +0100

    Fixed the layout of the migration file and added database-connectivity for the user-system.

--- backend/src/main/java/de/dhbw/tinf22b5/dinder/services/UserService.java
+++ backend/src/main/java/de/dhbw/tinf22b5/dinder/services/UserService.java
@@ -1,9 +1,10 @@
 package de.dhbw.tinf22b5.dinder.services;
 
+import de.dhbw.tinf22b5.dinder.entities.User;
 import de.dhbw.tinf22b5.dinder.models.LoginModel;
+import de.dhbw.tinf22b5.dinder.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,21 +12,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collection;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final SecurityService securityService;
 
-    private final Map<String, String> users = Map.of("admin", "admin", "user", "password");
-
     public boolean login(LoginModel model) {
-        return users.containsKey(model.loginName()) && users.get(model.loginName()).equals(model.password());
+        return userRepository.findById(model.loginName())
+                .map(User::getPwdHash)
+                .filter(value -> passwordEncoder.matches(model.password(), value))
+                .isPresent();
     }
 
     @Transactional
@@ -35,45 +36,7 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if(!users.containsKey(username)) {
-            throw new UsernameNotFoundException(username);
-        }
-
-        return new UserDetails() {
-            @Override
-            public Collection<? extends GrantedAuthority> getAuthorities() {
-                return new HashSet<>();
-            }
-
-            @Override
-            public String getPassword() {
-                return passwordEncoder.encode(users.get(username));
-            }
-
-            @Override
-            public String getUsername() {
-                return username;
-            }
-
-            @Override
-            public boolean isAccountNonExpired() {
-                return true;
-            }
-
-            @Override
-            public boolean isAccountNonLocked() {
-                return true;
-            }
-
-            @Override
-            public boolean isCredentialsNonExpired() {
-                return true;
-            }
-
-            @Override
-            public boolean isEnabled() {
-                return true;
-            }
-        };
+        return userRepository.findById(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
     }
 }
