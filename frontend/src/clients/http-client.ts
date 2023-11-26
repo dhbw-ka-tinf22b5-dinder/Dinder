89d30abd76415f67ead4954ea639db305f5b0ffc
huchsle <janifu02@gmail.com>
2023-11-26 12:56:22 +0100

    usedAxios

--- frontend/src/clients/http-client.ts
+++ frontend/src/clients/http-client.ts
@@ -1,30 +1,12 @@
 import {UserLogin,UserRegister} from "../types/general.types";
+import axios from "axios";
 export function login(user:UserLogin):Promise<number>{// later this will be a json
-    const headers: Headers = new Headers();
-    headers.set('Content-Type', 'application/json');
-    headers.set('Accept', 'application/json');
-    headers.set('Access-Control-Allow-Origin', '*');
-    const request:RequestInfo = new Request('http://localhost:8080/login', {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(user)
-    });
-    // @ts-ignore
-    return fetch(request)
-        .then(res=> res.status)
-        .catch(error => console.log(error));
+    return axios.post('http://localhost:8080/login', user)
+        .then(res => res.status)
+        .catch(error => error.status);
 }
-export function register(user:UserRegister):Promise<number>{// later this will be a json
-    const headers: Headers = new Headers();
-    headers.set('Content-Type', 'application/json');
-    headers.set('Accept', 'application/json');
-    const request:RequestInfo = new Request('http://localhost:8080/register', {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(user)
-    });
-    // @ts-ignore
-    return fetch(request)
-        .then(res=> res.status)
+export function register(user:UserRegister):Promise<number> {// later this will be a json
+    return axios.post('http://localhost:8080/register', user)
+        .then(res => res.status)
         .catch(error => error.status);
 }
