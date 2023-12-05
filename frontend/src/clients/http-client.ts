497a3cc041681608c3e2d30c672ddeae954784ae
Jannis Fuchs <janifu02@gmail>
2023-12-05 13:01:05 +0100

    changedAPIEndpoint

--- frontend/src/clients/http-client.ts
+++ frontend/src/clients/http-client.ts
@@ -1,14 +1,14 @@
 import {UserLogin,UserRegister} from "../types/general.types";
 import axios from "axios";
 
-export function login(user:UserLogin):Promise<unknown>{// later this will be a json
-    return axios.post('http://localhost:8080/login', user)
-        .then(res=> res)
+export function login(user:UserLogin):Promise<boolean>{// later this will be a json
+    return axios.post('http://localhost:8080/api/v1/login', user)
+        .then(res=> true)
 
         .catch(error => error.status);
 }
 export function register(user:UserRegister):Promise<number> {// later this will be a json
-    return axios.post('http://localhost:8080/register', user)
+    return axios.post('http://localhost:8080/api/v1/register', user)
         .then(res => res.status)
         .catch(error => error.status);
 }
