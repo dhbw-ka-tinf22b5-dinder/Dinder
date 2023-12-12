43a15ffbaa7a38cb562cb338d74cdeb0e020a63b
traible <traible@apollon.de>
2023-12-12 13:39:17 +0100

    Reformatted code

--- backend/src/main/java/de/dhbw/tinf22b5/dinder/controller/ReactController.java
+++ backend/src/main/java/de/dhbw/tinf22b5/dinder/controller/ReactController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ReactController {
-    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/*/{y:[\\w\\-]+}","/error"  })
+    @RequestMapping(value = {"/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/*/{y:[\\w\\-]+}", "/error"})
     public String getIndex() {
         //noinspection SpringMVCViewInspection
         return "/index.html";
