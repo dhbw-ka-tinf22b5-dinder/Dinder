80e8795da27756b3715ba1c0244a9b40fcd6ea2f
js-12345 <123568064+js-12345@users.noreply.github.com>
2024-04-24 12:59:41 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,15 @@ allprojects {
 subprojects {
     apply(plugin = "java")
 }
+
+plugins {
+  id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "dhbw-ka-tinf22b5-dinder_Dinder")
+    property("sonar.organization", "dhbw-ka-tinf22b5-dinder")
+    property("sonar.host.url", "https://sonarcloud.io")
+  }
+}
