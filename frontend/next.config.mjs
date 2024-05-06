26f5d28cb038491c000f415cb38f1f72909f4c9a
traible <traible@apollon.de>
2024-05-06 09:37:48 +0200

    Migrated the build configuration.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    distDir: '../backend/src/main/resources/public'
+};
 
 export default nextConfig;
