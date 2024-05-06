790885e8ad043c74ac9f7ff16db5f3b0b8f6893b
traible <traible@apollon.de>
2024-05-06 10:09:38 +0200

    Fixed the api documentation being deleted.

---
+++ frontend/src/build-api.js
@@ -0,0 +1,14 @@
+import fs from 'fs';
+
+main();
+
+function main() {
+    const prefix = '../backend/src/main/resources/'
+
+    let errCallback = err => {
+        if (err) return console.log(err);
+    }
+
+    fs.mkdir(prefix + 'public/api', errCallback);
+    fs.copyFile(prefix + 'api-docs/index.html', prefix + 'public/api/index.html', errCallback);
+}
