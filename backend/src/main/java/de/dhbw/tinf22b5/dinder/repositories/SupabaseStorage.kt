f7654dceba8e47a50415c6e89e52eb2a7659e676
traible <traible@apollon.de>
2024-05-08 15:02:41 +0200

    Slightly reworked the kotlin code to remove the opt-in.

--- backend/src/main/java/de/dhbw/tinf22b5/dinder/repositories/SupabaseStorage.kt
+++ backend/src/main/java/de/dhbw/tinf22b5/dinder/repositories/SupabaseStorage.kt
@@ -1,16 +1,16 @@
 package de.dhbw.tinf22b5.dinder.repositories
 
 import io.github.jan.supabase.storage.BucketApi
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.future.future
+import kotlinx.coroutines.runBlocking
 import java.util.concurrent.CompletableFuture
 
 class SupabaseStorage
 
-@OptIn(DelicateCoroutinesApi::class)
 fun BucketApi.uploadAsync(path: String, data: ByteArray, upsert: Boolean = false): CompletableFuture<String> {
-    return GlobalScope.future {
-        upload(path, data, upsert)
+    return runBlocking {
+        future {
+            upload(path, data, upsert)
+        }
     }
 }
