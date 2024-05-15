8267d32fb8b4d561fe170f957a928cad44d618b5
traible <traible@apollon.de>
2024-05-15 10:17:29 +0200

    Formatting + remove unused imports.

--- backend/src/test/java/de/dhbw/tinf22b5/dinder/UserControllerTest.java
+++ backend/src/test/java/de/dhbw/tinf22b5/dinder/UserControllerTest.java
@@ -2,17 +2,14 @@ package de.dhbw.tinf22b5.dinder;
 
 import de.dhbw.tinf22b5.dinder.services.UserService;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
@@ -20,11 +17,10 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @Testcontainers
 class UserControllerTest {
-    @LocalServerPort
-    private Integer port;
-
     @Autowired
     UserService userService;
+    @LocalServerPort
+    private Integer port;
 
     @BeforeEach
     void setUp() {
@@ -32,8 +28,7 @@ class UserControllerTest {
     }
 
     @Test
-    void shouldGive401Unauthorized(){
+    void shouldGive401Unauthorized() {
         when().get("/api/v1/user/me").then().statusCode(401);
     }
-
 }
