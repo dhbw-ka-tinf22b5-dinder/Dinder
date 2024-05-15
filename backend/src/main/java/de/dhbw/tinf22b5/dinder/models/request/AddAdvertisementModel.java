263de5b3c9ae4d2f66393707cc6baa29f51e5832
traible <traible@apollon.de>
2024-05-15 09:45:37 +0200

    Removed already resolved todo comment

--- backend/src/main/java/de/dhbw/tinf22b5/dinder/models/request/AddAdvertisementModel.java
+++ backend/src/main/java/de/dhbw/tinf22b5/dinder/models/request/AddAdvertisementModel.java
@@ -1,6 +1,5 @@
 package de.dhbw.tinf22b5.dinder.models.request;
 
-//TODO generate image, creation time
 public record AddAdvertisementModel(String title, double price, String location, int postalCode, String description) implements RequestModel {
     @Override
     public boolean isInvalid() {
