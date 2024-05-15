082e2aec80924c68b391d3dad2a8e80b615fd681
traible <traible@apollon.de>
2024-05-15 10:06:24 +0200

    Moved version numbers to variables.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -22,41 +22,59 @@ repositories {
     mavenLocal()
 }
 
+val jetbrainsAnnotationsVersion = "24.0.0"
+val lombokVersion = "1.18.28"
+val springBootVersion = "3.2.4"
+val postgresqlVersion = "42.6.0"
+val h2Version = "1.4.200"
+val flywayVersion = "10.0.1"
+val jwtVersion = "0.11.5"
+val gsonVersion = "2.10.1"
+val springDocVersion = "2.1.0"
+val supabaseStorageVersion = "2.3.1"
+val ktorVersion = "2.3.10"
+val kotlinxCoroutinesVersion = "1.8.0"
+val tikaVersion = "2.9.0"
+val restAssuredVersion = "5.4.0"
+val junitVersion = "4.13.1"
+val jupiterVersion = "5.8.1"
+val testContainersJupiterVersion = "1.19.8"
+
 dependencies {
-    implementation("org.jetbrains:annotations")
-    implementation("org.projectlombok:lombok")
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.boot:spring-boot-starter-tomcat")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.postgresql:postgresql")
-    implementation("com.h2database:h2")
-    implementation("org.flywaydb:flyway-core")
-    implementation("org.flywaydb:flyway-database-postgresql")
-    implementation("io.jsonwebtoken:jjwt-impl")
-    implementation("io.jsonwebtoken:jjwt-api")
-    implementation("io.jsonwebtoken:jjwt-gson")
-    implementation("com.google.code.gson:gson")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-api")
-    implementation("io.github.jan-tennert.supabase:storage-kt")
-    implementation("io.ktor:ktor-client-okhttp")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android")
-    implementation("org.apache.tika:tika-core")
+    implementation("org.jetbrains:annotations:$jetbrainsAnnotationsVersion")
+    implementation("org.projectlombok:lombok:$lombokVersion")
+    implementation("org.springframework.boot:spring-boot-starter$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-tomcat$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-web$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-security$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa$springBootVersion")
+    implementation("org.postgresql:postgresql:$postgresqlVersion")
+    implementation("com.h2database:h2:$h2Version")
+    implementation("org.flywaydb:flyway-core:$flywayVersion")
+    implementation("org.flywaydb:flyway-database-postgresql:$flywayVersion")
+    implementation("io.jsonwebtoken:jjwt-impl:$jwtVersion")
+    implementation("io.jsonwebtoken:jjwt-api:$jwtVersion")
+    implementation("io.jsonwebtoken:jjwt-gson:$jwtVersion")
+    implementation("com.google.code.gson:gson:$gsonVersion")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:$springDocVersion")
+    implementation("io.github.jan-tennert.supabase:storage-kt:$supabaseStorageVersion")
+    implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$kotlinxCoroutinesVersion")
+    implementation("org.apache.tika:tika-core:$tikaVersion")
 
-    annotationProcessor("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
 
-    testImplementation("io.rest-assured:rest-assured")
-    testImplementation("io.rest-assured:spring-mock-mvc")
-    testImplementation("org.springframework.boot:spring-boot-testcontainers")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("junit:junit")
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("io.rest-assured:rest-assured:$restAssuredVersion")
+    testImplementation("io.rest-assured:spring-mock-mvc:$restAssuredVersion")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers:$springBootVersion")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
+    testImplementation("junit:junit:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter:$jupiterVersion")
+    testImplementation("org.testcontainers:junit-jupiter:$testContainersJupiterVersion")
 }
 
 application {
