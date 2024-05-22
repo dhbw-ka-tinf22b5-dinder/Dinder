806b4f7f2ba491b16e7d570d14759e66ad268768
js-12345 <schaeferjonathan@gmx.net>
2024-05-22 14:16:06 +0200

    refactor(UserControllerTest): :recycle: added a more tests for the user controller

--- backend/src/test/java/de/dhbw/tinf22b5/dinder/UserControllerTest.java
+++ backend/src/test/java/de/dhbw/tinf22b5/dinder/UserControllerTest.java
@@ -1,18 +1,22 @@
 package de.dhbw.tinf22b5.dinder;
 
 import de.dhbw.tinf22b5.dinder.controller.UserController;
+import de.dhbw.tinf22b5.dinder.services.SecurityService;
 import de.dhbw.tinf22b5.dinder.services.UserService;
 import io.restassured.RestAssured;
+import io.restassured.matcher.DetailedCookieMatcher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @ActiveProfiles("dev")
@@ -21,6 +25,10 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 class UserControllerTest {
     @Autowired
     UserService userService;
+
+    @Autowired
+    SecurityService securityService;
+
     @LocalServerPort
     private Integer port;
 
@@ -41,17 +49,36 @@ class UserControllerTest {
                 contentType("application/json").
         when().post("/api/v1/register").
         then().
-                statusCode(409);
+                statusCode(HttpStatus.CONFLICT.value());
     }
 
     @Test
-    void registerTest() {
+    void registerSuccessTest() {
         given().
                 body("{\"email\":\"newUser@yahoo.de\",\"userName\":\"newUser\",\"password\":\"123\"}").
                 contentType("application/json").
         when().post("/api/v1/register").
         then().
-                statusCode(200).
+                statusCode(HttpStatus.OK.value()).
                 cookie(UserController.SESSION_ID_COOKIE);
+    }
+
+    @Test
+    void getUserInfo() {
+        given().
+                cookie(UserController.SESSION_ID_COOKIE, securityService.generateKey("mustermann@max.de")).
+        when().get("/api/v1/user/me").
+        then().
+                statusCode(HttpStatus.OK.value()).
+                body("userName", equalTo("maxMustermann"));
+    }
+
+    @Test
+    void getUserInfoUnregisteredUser() {
+        given().
+                cookie(UserController.SESSION_ID_COOKIE, securityService.generateKey("mustermann@max.de")).
+        when().get("/api/v1/user/not@registerd.user").
+        then().
+                statusCode(HttpStatus.BAD_REQUEST.value());
     }
 }
