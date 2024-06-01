679620d72df5b5a09795392760b7bbb5849b6be2
huchsle <janifu02@gmail.com>
2024-06-01 18:23:52 +0200

    fixed linter

--- frontend/src/components/universal/Navbar.tsx
+++ frontend/src/components/universal/Navbar.tsx
@@ -3,7 +3,7 @@
 import NavBarAppFunctionalityComponent from "@/components/universal/navBar_appFunctionality.component.tsx";
 import NavBarStartComponent from "@/components/universal/navBar_start.component.tsx";
 import { useSelector } from "react-redux";
-import { RootState } from "@/lib/store.ts";
+import type { RootState } from "@/lib/store.ts";
 
 const NavBar = () => {
 	const valueUser = useSelector((state: RootState) => state.login.userName);
