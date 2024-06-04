9faf422b7ee57b349987e1ced6039ab61e775a5e
traible <traible@apollon.de>
2024-06-04 17:29:20 +0200

    refactor(biome-linter): :rotating_light: Actually fixed the linting issues this time, IntelliJ didn't like the formatting that biome liked, but this time frfr.

--- frontend/src/lib/slices/advertisement.ts
+++ frontend/src/lib/slices/advertisement.ts
@@ -1,5 +1,5 @@
-import type {Advertisement} from "@/types/general.types.ts";
-import {createSlice, type PayloadAction} from "@reduxjs/toolkit";
+import type { Advertisement } from "@/types/general.types.ts";
+import { type PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 interface AdvertisementData {
 	Advertisement: Advertisement[];
