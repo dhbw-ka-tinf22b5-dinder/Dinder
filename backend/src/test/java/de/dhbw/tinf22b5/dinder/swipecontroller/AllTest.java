e2e5ced7d11cbcb9acd5b91c05025911994d2f63
traible <traible@apollon.de>
2024-06-06 08:47:34 +0200

    add(all-swipe-test): :white_check_mark: added tests for the swipe information controller

---
+++ backend/src/test/java/de/dhbw/tinf22b5/dinder/swipecontroller/AllTest.java
@@ -0,0 +1,79 @@
+package de.dhbw.tinf22b5.dinder.swipecontroller;
+
+import de.dhbw.tinf22b5.dinder.controller.UserController;
+import de.dhbw.tinf22b5.dinder.services.SecurityService;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@ActiveProfiles("dev")
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@Testcontainers
+public class AllTest {
+    @Autowired
+    SecurityService securityService;
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    void noSessionToken() {
+        when().get("/api/v1/advertisement/2/swipe/all").
+                then().
+                statusCode(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    @Test
+    void getEmptyAllSwipes() {
+        String response =
+                given()
+                        .cookie(UserController.SESSION_ID_COOKIE, securityService.generateKey("mustermann@max.de"))
+                        .when().get("/api/v1/advertisement/1/swipe/all")
+                        .then()
+                        .statusCode(HttpStatus.OK.value())
+                        .extract().body().asPrettyString();
+
+        Assertions.assertEquals(JsonPath.from("[]").prettify(), response);
+    }
+
+    @Test
+    void getAllSwipes() {
+        JsonPath response =
+                given().
+                        cookie(UserController.SESSION_ID_COOKIE, securityService.generateKey("mustermann@max.de")).
+                        when().get("/api/v1/advertisement/2/swipe/all").
+                        then().
+                        statusCode(HttpStatus.OK.value()).
+                        extract().body().jsonPath();
+
+        JsonPath expected = JsonPath.from("[{\"swipeId\":1,\"swipeTime\":\"2023-11-13T08:09:10Z\"," +
+                "\"swipeState\":\"ACCEPTED\",\"advertisement\":{\"title\":\"Haus bauen\",\"price\":17.5," +
+                "\"location\":\"Karlsruhe\",\"postalCode\":12345,\"description\":\"description123\"," +
+                "\"imagePath\":null,\"advertiser\":{\"userName\":\"maxMustermann\"}," +
+                "\"creationTime\":\"2023-11-12T08:09:10Z\"},\"user\":{\"userName\":\"userTest\"}},{\"swipeId\": 2," +
+                "\"swipeTime\":\"2023-11-12T08:09:11Z\",\"swipeState\":\"ACCEPTED\"," +
+                "\"advertisement\":{\"title\":\"Haus bauen\",\"price\":17.5,\"location\":\"Karlsruhe\"," +
+                "\"postalCode\":12345,\"description\":\"description123\",\"imagePath\":null," +
+                "\"advertiser\":{\"userName\":\"maxMustermann\"},\"creationTime\":\"2023-11-12T08:09:10Z\"}," +
+                "\"user\":{\"userName\":\"user123\"}}]");
+
+        Assertions.assertEquals(expected.prettify(), response.prettify());
+    }
+}
