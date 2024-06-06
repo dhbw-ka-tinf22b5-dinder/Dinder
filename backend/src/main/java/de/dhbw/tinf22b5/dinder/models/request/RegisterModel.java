0c04866822a7f903277146355c8caca7994bbb26
traible <traible@apollon.de>
2024-06-06 09:42:49 +0200

    Fixed the regex to allow uppercase in email usernames.

--- backend/src/main/java/de/dhbw/tinf22b5/dinder/models/request/RegisterModel.java
+++ backend/src/main/java/de/dhbw/tinf22b5/dinder/models/request/RegisterModel.java
@@ -3,8 +3,8 @@ package de.dhbw.tinf22b5.dinder.models.request;
 import java.util.regex.Pattern;
 
 public record RegisterModel(String email, String userName, String password) implements RequestModel {
-    private static final Pattern EMAIL_REGEX = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\" +
-            ".[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f" +
+    private static final Pattern EMAIL_REGEX = Pattern.compile("(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\" +
+            ".[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f" +
             "]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9]" +
             "(?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}" +
             "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:" +
