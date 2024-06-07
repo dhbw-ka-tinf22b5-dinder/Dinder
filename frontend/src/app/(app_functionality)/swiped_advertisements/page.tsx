7a40102841bee95485e6762a823823f13330f1c3
traible <traible@apollon.de>
2024-06-07 21:08:35 +0200

    Fixed incorrect revert.

--- frontend/src/app/(app_functionality)/swiped_advertisements/page.tsx
+++ frontend/src/app/(app_functionality)/swiped_advertisements/page.tsx
@@ -1,11 +1,7 @@
 "use client";
-import { Button } from "@/components/atoms/Button.component.tsx";
-import {
-	CardGrid,
-	CardGridItem,
-	InformationImage,
-} from "@/styles/advertisementManagement.styles.ts";
-import { Info } from "@/styles/swipecard.styles.ts";
+import {Button} from "@/components/atoms/Button.component.tsx";
+import {CardGrid, CardGridItem, InformationImage,} from "@/styles/advertisementManagement.styles.ts";
+import {Info} from "@/styles/swipecard.styles.ts";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import style from "../Advertisement.module.css";
@@ -162,8 +158,7 @@ function Advertisement({
 					<LocationOnIcon /> {advertisement.plz} {advertisement.location}
 				</p>
 				<p>
-					<CalendarMonthIcon />{" "}
-					{advertisement.creationTime.toLocaleDateString()}
+                    <CalendarMonthIcon /> {advertisement.creationTime}
 				</p>
 			</Info>
 			<label className={style.state}>Pending</label>
