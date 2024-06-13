934403d5516d5e1d67f3c92f79eeafbd10b6a42f
traible <traible@apollon.de>
2024-06-13 21:22:00 +0200

    Fixed Spring sometimes not starting correctly.

--- backend/src/main/java/de/dhbw/tinf22b5/dinder/DinderApplication.java
+++ backend/src/main/java/de/dhbw/tinf22b5/dinder/DinderApplication.java
@@ -5,9 +5,13 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("de.dhbw.tinf22b5.dinder.")
+@EnableJpaRepositories("de.dhbw.tinf22b5.dinder.*")
+@ComponentScan(basePackages = { "de.dhbw.tinf22b5.dinder.*" })
+@EntityScan("de.dhbw.tinf22b5.dinder.*")
 @OpenAPIDefinition(info = @Info(title = "Dinder", version = "v1"))
 public class DinderApplication {
     @ExcludeFromJacocoGeneratedReport
